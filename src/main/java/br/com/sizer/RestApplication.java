1c3c07d2a120eac96b2ed12bae36c96452f240b3
André Miani <andremiani@gmail.com>
2024-06-28 16:10:59 -0300

    fix: env file

--- src/main/java/br/com/sizer/RestApplication.java
+++ src/main/java/br/com/sizer/RestApplication.java
@@ -12,6 +12,15 @@ public class RestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApplication.class, args);
+
+		// Exemplo de como imprimir variáveis de ambiente
+		String databaseUrl = System.getenv("DATABASE_URL");
+		String databaseUsername = System.getenv("DATABASE_USERNAME");
+		String databasePassword = System.getenv("DATABASE_PASSWORD");
+
+		System.out.println("DATABASE_URL: " + databaseUrl);
+		System.out.println("DATABASE_USERNAME: " + databaseUsername);
+		System.out.println("DATABASE_PASSWORD: " + databasePassword);
 	}
 
 }
